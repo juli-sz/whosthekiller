44d6a17ea5a5cee37141c93e7c7d803e45adfaab
juli-sz <jcdsaez@gmail.com>
2025-06-04 04:55:07 -0300

    Revert "incorporacion de un workflow de pruebas unitarias"
    This reverts commit c055c45e144ac35cf8673f1cb740fd7d4b8c2fbe.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+import Juego from './models/Juego.js';
+
+import { agregarCategorias, agregarItem, agregarJugadores, editarJuego } from './funciones/ui.js';
+import { cargarDatosIniciales } from './funciones/datosIniciales.js';
+
+export const juegoNuevo = new Juego();
+cargarDatosIniciales(juegoNuevo);
+
+function inicioJuego() {
+  juegoNuevo.iniciarJuego();
+  juegoNuevo.jugadores.forEach(j => j.mostrarInformacion());
+  juegoNuevo.mostrarInformacionSecreta();
+}
+
+function verMarcador() {
+  alert("Ganadores anteriores:\n" + juegoNuevo.ganadores.join(", "));
+}
+
+$(document).ready(function () {
+  $("#botonJugar").click(inicioJuego);
+  $("#botonMarcador").click(verMarcador);
+  $("#botonEditar").click(editarJuego);
+  $("#botonJugadores").click(agregarJugadores);
+  $("#botonAgregarCategorias").click(agregarCategorias);
+  $("#botonAgregarItem").click(agregarItem);
+});
